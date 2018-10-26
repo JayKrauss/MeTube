ff77beac93f922857e95a6a573ac6a29b183f0b0
Jay <paxformatus@gmail.com>
2018-10-26 14:18:48 -0400

    Updates

---
+++ precache-manifest.533ec3b1c302348c0b36b21f9c53545a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MeTube/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "afe945ebd12a37585173",
+    "url": "/MeTube/static/js/runtime~main.afe945eb.js"
+  },
+  {
+    "revision": "1fad7749e10937fb1b62",
+    "url": "/MeTube/static/js/main.1fad7749.chunk.js"
+  },
+  {
+    "revision": "3700415acf8b34a939a7",
+    "url": "/MeTube/static/js/1.3700415a.chunk.js"
+  },
+  {
+    "revision": "1fad7749e10937fb1b62",
+    "url": "/MeTube/static/css/main.cc84bfee.chunk.css"
+  },
+  {
+    "revision": "25000fd72cb84057af003b07969cfea0",
+    "url": "/MeTube/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MeTube/precache-manifest.533ec3b1c302348c0b36b21f9c53545a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MeTube/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
